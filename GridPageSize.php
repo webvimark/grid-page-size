fe0a3e2e1d6eedef9ac710c98271c8d119367386
vi mark <webvimark@gmail.com>
2014-06-23 23:20:38 +0200

    ogogo

---
+++ GridPageSize.php
@@ -0,0 +1,75 @@
+<?php
+namespace webvimark\extensions\GridPageSize;
+
+use yii\base\InvalidConfigException;
+use yii\base\Widget;
+use yii\helpers\Url;
+
+class GridPageSize extends Widget
+{
+
+	/**
+	 * @var string
+	 */
+	public $pjaxId;
+
+	/**
+	 * @var array
+	 */
+	public $dropDownOptions;
+
+	/**
+	 * @var string
+	 */
+	public $text = 'Записей на странице';
+
+	/**
+	 * @throws \yii\base\InvalidConfigException
+	 * @return string
+	 */
+	public function run()
+	{
+		if ( ! $this->pjaxId )
+		{
+			throw new InvalidConfigException('Missing pjaxId param');
+		}
+
+		$this->setDefaultOptions();
+
+		$this->view->registerJs($this->js());
+
+		return $this->render('index');
+	}
+
+	/**
+	 * Set default options
+	 */
+	protected function setDefaultOptions()
+	{
+		if ( ! $this->dropDownOptions )
+		{
+			$this->dropDownOptions = [5=>5, 10=>10, 20=>20, 50=>50, 100=>100, 200=>200];
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function js()
+	{
+		$pjaxId = '#' . ltrim($this->pjaxId, '#');
+
+		$js = <<<JS
+			$(document).on('change', '[name="grid-page-size"]', function () {
+				var _t = $(this);
+				$.post('$this->url', { 'grid-page-size': _t.val() })
+					.done(function(){
+						$.pjax.reload({container: '$pjaxId'})
+					});
+			});
+JS;
+
+		return $js;
+
+	}
+} 
---
+++ views/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @var View $this
+ */
+?>
+<?php
+use yii\helpers\Html;
+use yii\web\View;
+
+?>
+<div class="form-inline pull-right" style="margin-top: -60px">
+	<?= $this->context->text ?>
+	<?= Html::dropDownList(
+		'grid-page-size', \Yii::$app->request->cookies->getValue('_grid_page_size', 20),
+		$this->context->dropDownOptions,
+		['class'=>'form-control']
+	) ?>
+</div>
