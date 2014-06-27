7ace01c1f206f9da2d9c713482e693431880e1f8
vi mark <webvimark@gmail.com>
2014-06-27 17:09:54 +0200

    style imporvements

--- views/index.php
+++ views/index.php
@@ -8,11 +8,11 @@ use yii\helpers\Html;
 use yii\web\View;
 
 ?>
-<div class="form-inline pull-right" style="margin-top: -60px">
+<div class="form-inline pull-right" style="margin-top: -5px">
 	<?= $this->context->text ?>
 	<?= Html::dropDownList(
 		'grid-page-size', \Yii::$app->request->cookies->getValue('_grid_page_size', 20),
 		$this->context->dropDownOptions,
-		['class'=>'form-control']
+		['class'=>'form-control input-sm']
 	) ?>
 </div>
