42057d4f82320224e11e69f23be44c185026f186
66d27b3ea53eda58ad7f71253dc2338b08efe4c4 <botrelli@gmx.com>
2014-08-03 05:53:08 -0400

    CS Fixes

--- views/index.php
+++ views/index.php
@@ -11,8 +11,8 @@ use yii\web\View;
 <div class="form-inline pull-right" style="margin-top: -5px">
 	<?= $this->context->text ?>
 	<?= Html::dropDownList(
-		'grid-page-size', \Yii::$app->request->cookies->getValue('_grid_page_size', 20),
-		$this->context->dropDownOptions,
-		['class'=>'form-control input-sm']
-	) ?>
+        'grid-page-size', \Yii::$app->request->cookies->getValue('_grid_page_size', 20),
+        $this->context->dropDownOptions,
+        ['class'=>'form-control input-sm']
+    ) ?>
 </div>
